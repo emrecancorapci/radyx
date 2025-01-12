b9d0894403e17244a92898bafadcf22157b7d94f
Emre Can Çorapçı <emrecancorapci@gmail.com>
2025-01-12 16:55:09 +0300

    Update to implement splitting leaf partially

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,7 @@ impl<'a, T: Debug> RadixNode<'a, T> {
 
     pub fn add(&mut self, path: &'a str, val: Option<T>) -> Result<(), String> {
         let mut split_string = "";
+
         if self.nodes.is_empty() {
             self.nodes.insert(path, Box::new(RadixNode::new(val)));
             return Ok(());
@@ -28,20 +29,40 @@ impl<'a, T: Debug> RadixNode<'a, T> {
         }
 
         for (key, node) in &mut self.nodes {
-            if path.len() > key.len() {
+            if key.is_empty() {
+                continue;
+            }
+
+            let splitting_index = find_common_index(path, key);
+
+            if splitting_index == 0 {
+                continue;
+            }
+
+            if splitting_index == key.len() {
                 if path.starts_with(key) {
                     return node.add(&path[key.len()..], val);
                 }
 
-                for (index, ch) in key.chars().enumerate(){
-                    
-                }
-            } else if path.len() < key.len() {
+                let (base_key, splitted_key) = path.split_at(splitting_index);
+                let _ = self.split_node(base_key, splitted_key);
+
+                return self.add(path, val);
+            }
+
+            if splitting_index == path.len() {
                 if key.starts_with(path) {
                     split_string = key;
+                    break;
                 }
-                break;
-            } else if &path == key {
+
+                let (base_key, splitted_key) = key.split_at(splitting_index);
+                let _ = self.split_node(base_key, splitted_key);
+
+                return self.add(path, val);
+            }
+
+            if &path == key {
                 match node.val {
                     Some(ref old_val) => return Err(format!("Value is not None, {:?}", old_val)),
                     None => {
@@ -50,6 +71,11 @@ impl<'a, T: Debug> RadixNode<'a, T> {
                     }
                 }
             }
+
+            let (base_key, splitted_key) = key.split_at(splitting_index);
+            let _ = self.split_node(base_key, splitted_key);
+
+            return self.add(path, val);
         }
 
         if !split_string.is_empty() {
@@ -64,7 +90,10 @@ impl<'a, T: Debug> RadixNode<'a, T> {
     }
 
     fn split_node(&mut self, base_key: &'a str, splitted_key: &'a str) -> Result<(), String> {
-        let node = self.nodes.remove(format!("{base_key}{splitted_key}").as_str()).unwrap();
+        let node = self
+            .nodes
+            .remove(format!("{base_key}{splitted_key}").as_str())
+            .unwrap();
 
         let mut main_node = RadixNode::new(None);
 
@@ -87,7 +116,7 @@ impl<'a, T: Debug> RadixNode<'a, T> {
             if key == &path {
                 if let Some(val) = &node.val {
                     return Some(val);
-                } 
+                }
                 return None;
             }
             if path.starts_with(key) {
@@ -102,4 +131,18 @@ impl<'a, T: Debug> Default for RadixNode<'a, T> {
     fn default() -> Self {
         Self::new(None)
     }
+}
+
+pub fn find_common_index(s1: &str, s2: &str) -> usize {
+    let mut iter1 = s1.chars();
+    let mut iter2 = s2.chars();
+
+    for i in 0.. {
+        match (iter1.next(), iter2.next()) {
+            (Some(ch1), Some(ch2)) if ch1 == ch2 => continue,
+            _ => return i,
+        }
+    }
+
+    0
 }
