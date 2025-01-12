9861c270650d70bdc34370881a69484424af4acf
Emre Can Çorapçı <emrecancorapci@gmail.com>
2025-01-12 14:43:15 +0300

    Update add function to implement basic functions

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::Debug};
 
 struct RadixTree {}
 
 #[derive(Debug)]
-pub struct RadixNode<'a, T> {
+pub struct RadixNode<'a, T: Debug> {
     nodes: HashMap<&'a str, Box<RadixNode<'a, T>>>,
     val: Option<T>,
 }
 
-impl<'a, T> RadixNode<'a, T> {
+impl<'a, T: Debug> RadixNode<'a, T> {
     fn new(val: Option<T>) -> RadixNode<'a, T> {
         RadixNode {
             nodes: HashMap::new(),
@@ -16,66 +16,67 @@ impl<'a, T> RadixNode<'a, T> {
         }
     }
 
-    pub fn add(&mut self, path: &'a str, val: Option<T>) -> Result<(), ()> {
+    pub fn add(&mut self, path: &'a str, val: Option<T>) -> Result<(), String> {
         let mut split_string = "";
         if self.nodes.is_empty() {
             self.nodes.insert(path, Box::new(RadixNode::new(val)));
             return Ok(());
-        }
-
-        if path.is_empty() {
+        } else if path.is_empty() {
             self.val = val;
             return Ok(());
         }
 
         for (key, node) in &mut self.nodes {
-            // key - path
-            // equal -> check val none -> if true add
-            // not equal -> check starts with
-            //  -> if true add remain str
-            //  -> add
-            if &path == key {
-                match self.val {
-                    Some(_) => return Err(()),
+            if path.len() > key.len() {
+                if path.starts_with(key) {
+                    return node.add(&path[key.len()..], val);
+                }
+
+                for (index, ch) in key.chars().enumerate(){
+                    
+                }
+            } else if path.len() < key.len() {
+                if key.starts_with(path) {
+                    split_string = key;
+                }
+                break;
+            } else if &path == key {
+                println!("THIS {} {}", path, key);
+                match node.val {
+                    Some(ref old_val) => return Err(format!("Value is not None, {:?}", old_val)),
                     None => {
-                        self.val = val;
+                        node.set_value(val);
                         return Ok(());
                     }
                 }
             }
-            if path.starts_with(key) {
-                return node.add(&path[key.len() - 1..], val);
-            }
-            if key.starts_with(path) {
-                let (new_key, second_key) = key.split_at(path.len() - 1);
-                let mut main_node = RadixNode::new(None);
-    
-                main_node.add(second_key, node.val);
-                main_node.add("", val);
-    
-                self.nodes.insert(new_key, Box::new(main_node));
-    
-                return Ok(());
-                split_string = key;
-                break;
-            }
         }
 
-        // if !split_string.is_empty() {
-        //     let (key, node) self.nodes.get(split_string);
-        //     let (new_key, second_key) = key.split_at(path.len() - 1);
-        //     let mut main_node = RadixNode::new(None);
+        if !split_string.is_empty() {
+            let (base_key, splitted_key) = split_string.split_at(path.len());
+            let _ = self.split_node(base_key, splitted_key);
 
-        //     main_node.add(second_key, node.val);
-        //     main_node.add("", val);
-
-        //     self.nodes.insert(new_key, Box::new(main_node));
-
-        //     return Ok(());
-        // }
+            println!("Splitted and Add: {} {:?}", path, &val);
+            return self.add(path, val);
+        }
 
         self.nodes.insert(path, Box::new(RadixNode::new(val)));
         return Ok(());
+    }
+
+    fn split_node(&mut self, base_key: &'a str, splitted_key: &'a str) -> Result<(), String> {
+        let node = self.nodes.remove(format!("{base_key}{splitted_key}").as_str()).unwrap();
+
+        let mut main_node = RadixNode::new(None);
+
+        main_node.add(splitted_key, node.val)?;
+
+        self.nodes.insert(base_key, Box::new(main_node));
+        return Ok(());
+    }
+
+    fn set_value(&mut self, val: Option<T>) {
+        self.val = val;
     }
 
     pub fn get(&self, path: &'a str) -> Option<&T> {
@@ -87,25 +88,28 @@ impl<'a, T> RadixNode<'a, T> {
             if key == &path {
                 if let Some(val) = &node.val {
                     return Some(val);
-                }
-
+                } 
                 return None;
             }
             if path.starts_with(key) {
-                return node.get(&path[key.len() - 1..]);
+                return node.get(&path[key.len()..]);
             }
         }
-
         return None;
     }
 }
 
-impl<'a, T> Default for RadixNode<'a, T> {
+impl<'a, T: Debug> Default for RadixNode<'a, T> {
     fn default() -> Self {
         Self::new(None)
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut node: RadixNode<'_, String> = RadixNode::default();
+
+    node.add("/home", Some(String::from("Home")));
+    let val = node.get("/home");
+
+    println!("{}", val.unwrap())
 }
